4ad15f519a6766ea4d82a396dc0d9c2184f5502c
brandenk98 <36284259+brandenk98@users.noreply.github.com>
2018-09-21 15:07:19 -0400

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -2,5 +2,39 @@
 #include <stdlib.h>
 #include <meregsort.h>
 
+struct Node{   // to store a linked list in the data structure
+  int data;
+  struct Node* next;
+}
 
+
+        // mergesort for numric and dateTime
+int a = numeric->data;
+int b = dateTime->data;
+
+String Node* mergesort( struct Node* a, struct Node* b){
+  if(a == Null){
+    return b;
+  }else{
+    if(b == Null){
+      return a;
+    }
+    struct Nude* result = NUll;
+    if( a->data <= b->data){
+      result = a;
+      result->next = mergesort(a->next, b);
+    }else{
+      result = b;
+      result->next = mergesort(b->next, a);
+    }
+    return result;
+  }
+  // mergesort implemented without split
+  
+  String Node* mergeSPlit(){
+    return 0;
+  }
+
+
+  
 
