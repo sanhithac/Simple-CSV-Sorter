1e576f2eb470d4e454712d398a3c55ab97e28558
sanhithac <sanhitha.c@gmail.com>
2018-09-25 11:45:08 -0400

    Update simpleCSVsorter.c

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -1,3 +1,10 @@
 #include <stdlib.h>
-#include <simpleCSVsorter.h>
+#include "simpleCSVsorter.h"
+#include "mergesort.c"
 
+int main(int argc, char* argv[]){
+  readfile();
+  merge();
+  print();
+  return 0;
+}
