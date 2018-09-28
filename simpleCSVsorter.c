01f4094d10cfe66bc0d3003efb65aee94af0c7be
sanhithac <sanhitha.c@gmail.com>
2018-09-28 17:02:21 -0400

    Update simpleCSVsorter.c

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -7,7 +7,9 @@ int main(int argc, char* argv[]){
     printf("Invalid Input");
     return 0;
   }
-  readfile(argv[2]);//read file and organize rows into nodes
+  Node *head=readfile(argv[2]);//read file and organize rows into nodes
+  if(head==NULL)
+     return 0;
   SortedMerge();//sorts the nodes/rows
   print(Node *head);//prints the sorted nodes to stdout
   return 0;
@@ -17,7 +19,10 @@ Node readfile(char *col[]){
   FILE *fp;
   char firstrow[600];
   fscanf(fp, "%[^\n]", firstrow);
-  columnNum(firstrow, col[]);
+  int colInd=columnNum(firstrow, col[]);
+  if(colInd==-1){
+    printf("Invalid Input");
+    return NULL;
   while(
   
 int columnNum(Char *row[], char *col[]){
@@ -25,8 +30,17 @@ int columnNum(Char *row[], char *col[]){
   const char s[2]=",";
   char *token;
   token=strtok(row, s);
+  int count=-1;
   token=remove_leading_spaces(token);
-  
+  while(token!=NULL){
+    if(strcmp(token, col)==0){
+      count++;
+      return count;
+    }
+    count++;
+  }
+  return count;
+}
   
 static void remove_leading_spaces(char* line) 
 {   
