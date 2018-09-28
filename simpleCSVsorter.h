780ce6ee664d6e51a8d2a7700cbda09c24b36960
sanhithac <sanhitha.c@gmail.com>
2018-09-28 17:02:28 -0400

    Update simpleCSVsorter.h

--- simpleCSVsorter.h
+++ simpleCSVsorter.h
@@ -1,35 +1,10 @@
 #include <stdlib.h>
 
 typedef struct{
-  char color[];
-  char director_name[];
-  int num_critic_for_reviews;
-  int duration; /*dateTime*/
-  int director_facebook_likes;
-  int actor_3_facebook_likes;
-  char actor_2_name[];
-  int actor_1_facebook_likes;
-  int gross;
-  char genres[];
-  char actor_1_name[];
-  char movie_title[];
-  int num_voted_users;
-  int cats_total_facebook_likes;
-  char actor_3_name[];
-  int facenumber_in_poster;
-  char plot_keywords[];
-  char movie_imdb_link[];
-  int num_user_for_reviews;
-  char language[];
-  char country[];
-  char content_ration[];
-  int budget;
-  int title_year;
-  int actor_2_facebook_likes;
-  int imdb_score;
-  int aspect_ratio;
-  int movie_facebook_likes;
-}node;
+  void *data;
+  char[] *row;
+  Node *next;
+}Node;
 
 /*int vals[28][2]={
   {"color", "String"},
