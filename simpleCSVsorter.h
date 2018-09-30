3cefe40cea6acc4da6d293637e66beba2a2a3e6f
sanhithac <sanhitha.c@gmail.com>
2018-09-29 21:40:57 -0400

    Update simpleCSVsorter.h

--- simpleCSVsorter.h
+++ simpleCSVsorter.h
@@ -40,6 +40,7 @@ typedef struct{
 void readfile();
 char *remove_leading_spaces(char* line);
 int columnNum(char *row, char *col);
+char *tokenizer(int col, char *line);
 String Node* SortedMerge( struct Node* a, struct Node* b);
 void print(struct Node* a);
 
