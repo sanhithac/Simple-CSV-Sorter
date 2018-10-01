4a43e12b2b3efc9e6d4ea2373b467706e553292b
sanhithac <sanhitha.c@gmail.com>
2018-09-30 20:34:17 -0400

    Update simpleCSVsorter.h

--- simpleCSVsorter.h
+++ simpleCSVsorter.h
@@ -10,5 +10,7 @@ Node* readfile(int colInd, FILE *fp);
 char *remove_leading_spaces(char* str);
 int columnNum(char *row, char *col);
 char *tokenizer(int col, char *line);
+void FrontBackSplit (Node* source, Node** frontRef, Node** backRef);
+void MergeSort (Node* headRef);
 Node* SortedMerge( Node* a, Node* b);
 void print( Node* a);
